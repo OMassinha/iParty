3ad0177cf193b63601cb5c8f0de0c889349df067
OMassinha <65530477+OMassinha@users.noreply.github.com>
2024-02-05 14:50:35 -0300

    Delete iParty directory

--- iParty/app_iParty/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    return render(request, 'homepage.html')
-def registrar(request):
-    return render(request, 'registrar.html')
-        
